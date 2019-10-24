dcb26754306d61c4c7aa11a566f460ed0cd3a665
lzh <598665303@qq.com>
2019-10-24 22:53:18 +0800

    img

--- src/servlet/UploadImageServlet.java
+++ src/servlet/UploadImageServlet.java
@@ -27,15 +27,14 @@ public class UploadImageServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
 	}
 
 }
